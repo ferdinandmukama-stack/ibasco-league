1d8d3e1ea28c335b3fb5fe7da46db172f630a8c7
ferdinandmukama-stack <ferdinandmukama@gmail.com>
2026-05-04 13:21:16 +0300

    Delete ibasco_league_app directory

--- ibasco_league_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Team, Fixture
-
-admin.site.register(Team)
-admin.site.register(Fixture)
-from .models import TopScorer
-admin.site.register(TopScorer)
